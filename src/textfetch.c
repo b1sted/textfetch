c303171f361cec9c751938407d73da836ba059d9
Nikita Mandrykin <189896017+b1sted@users.noreply.github.com>
2026-01-24 15:56:10 +0300

    feat: add shell information output

--- src/textfetch.c
+++ src/textfetch.c
@@ -1,3 +1,5 @@
+#define _POSIX_C_SOURCE 200809L
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,6 +11,7 @@
 #include <sys/sysinfo.h>
 #include <sys/types.h>
 #include <sys/utsname.h>
+#include <sys/wait.h>
 
 #define BUFFER_SIZE 512
 #define ANSI_BOLD   "\x1b[1m"
@@ -54,6 +57,18 @@ int get_distro_name(char *dest_buffer, const size_t dest_len);
  */
 void format_uptime(long uptime, char *dest_buffer, const size_t dest_len);
 
+/**
+ * Retrieves the version string of the parent process's shell.
+ * It identifies the shell binary via /proc, executes it with the '--version' flag,
+ * and captures the first line of the standard output.
+ *
+ * @param ppid The process ID of the parent process (used to determine the shell name).
+ * @param dest_buffer The buffer where the resulting version string will be stored.
+ * @param dest_len The maximum size of the destination buffer.
+ * @return Returns 0 on success, or a non-zero error code on failure.
+ */
+int get_parent_shell_name(pid_t ppid, char *dest_buffer, const size_t dest_len);
+
 int main(void) {
     struct utsname machine_info;
 
@@ -97,12 +112,16 @@ int main(void) {
     char procs_str[BUFFER_SIZE] = {0};
     snprintf(procs_str, sizeof(procs_str), "%u", system_info.procs);
 
+    pid_t parent_pid = getppid();
+    char shell_name[BUFFER_SIZE] = {0};
+    if (get_parent_shell_name(parent_pid, shell_name, BUFFER_SIZE) != 0) return 1;
+
     print_header(username, nodename, is_a_tty);
     print_information("OS", distro_name, is_a_tty);
     print_information("Kernel", machine_info.release, is_a_tty);
     print_information("Uptime", uptime_str, is_a_tty);
     print_information("Processes", procs_str, is_a_tty);
-
+    print_information("Shell", shell_name, is_a_tty);
     return 0;
 }
 
@@ -191,4 +210,75 @@ void format_uptime(long uptime, char *dest_buffer, const size_t dest_len) {
     } else {
         snprintf(dest_buffer, dest_len, "%02ld:%02ld:%02ld", hours, minutes, seconds);
     }
+}
+
+int get_parent_shell_name(pid_t ppid, char *dest_buffer, const size_t dest_len) {
+    char shell_proc_path[BUFFER_SIZE] = {0};
+    snprintf(shell_proc_path, sizeof(shell_proc_path), "/proc/%d/comm", ppid);
+
+    FILE *shell_proc_file = fopen(shell_proc_path, "r");
+    if (!shell_proc_file) {
+        perror("fail to open comm file");
+        return -1;
+    }
+
+    char buffer[BUFFER_SIZE];
+    if (fgets(buffer, BUFFER_SIZE, shell_proc_file) == NULL) {
+        fclose(shell_proc_file);
+        return -1;
+    }
+    fclose(shell_proc_file);
+
+    buffer[strcspn(buffer, "\n")] = 0;
+
+    char binary_path[BUFFER_SIZE] = "/usr/bin/";
+    strncat(binary_path, buffer, sizeof(binary_path) - strlen(binary_path) - 1);
+    
+    int pipefd[2]; // [0] - read, [1] - write
+
+    if (pipe(pipefd) == -1) {
+        perror("pipe failed");
+        return 1;
+    }
+
+    pid_t pid = fork();
+    
+    if (pid == -1) {
+        perror("fork failed");
+        return 1;
+    } else if (pid == 0) {
+        close(pipefd[0]);
+
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(pipefd[1]);
+
+        execlp(binary_path, buffer, "--version", NULL);
+
+        perror("exec failed");
+        exit(1); 
+    } else {
+        close(pipefd[1]);
+        
+        FILE *stream = fdopen(pipefd[0], "r");
+        if (!stream) {
+            perror("fdopen");
+            close(pipefd[0]);
+            wait(NULL);
+            return -1;
+        }
+
+        if (fgets(dest_buffer, dest_len, stream) != NULL) {
+            size_t len = strlen(dest_buffer);
+            if (len > 0 && dest_buffer[len - 1] == '\n') {
+                dest_buffer[len - 1] = '\0';
+            }
+        } else {
+            dest_buffer[0] = '\0';
+        }
+
+        fclose(stream);
+        wait(NULL);
+    }
+
+    return 0;
 }
